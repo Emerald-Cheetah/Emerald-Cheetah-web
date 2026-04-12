bd2fb90d5001ea1c103739e50a5e7856fff90ceb
Juanita Corsino <cocojuanasllc@gmail.com>
2026-04-12 03:57:10 -0400

    completed frontend tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+describe('The main app', () => {
+  it('should render the Home link', () => {
+    render(<App />);
+    expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
+  });
+
+  it('should render the Payment link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByRole('link', { name: 'Payment' })).toBeInTheDocument();
+});
+
+  it('should render the Catalog link', () => {
+    render(<App />);
+    expect(screen.getByRole('link', { name: 'Catalog' })).toBeInTheDocument();
+  });
+
+  it('should render the header title', () => {
+    render(<App />);
+    expect(screen.getByText('Emerald Cheetah')).toBeInTheDocument();
+  });
 });
